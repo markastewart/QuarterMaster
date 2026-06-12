e884f12d1250f47a751711e5144e13154140d2e7
Mark Stewart <mas8771@me.com>
2026-06-12 19:47:29 -0400

    Initial Commit

---
+++ QuarterMaster/QuarterMasterApp.swift
@@ -0,0 +1,32 @@
+//
+//  QuarterMasterApp.swift
+//  QuarterMaster
+//
+//  Created by Mark A Stewart on 6/12/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QuarterMasterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
