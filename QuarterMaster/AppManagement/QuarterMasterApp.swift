14686a4734c14b41af294f7bf0a8625ef5ef2311
yen4life <mark@yenforlife.com>
2026-06-15 16:50:41 -0400

    Clean up data model and clear warnings

--- QuarterMaster/AppManagement/QuarterMasterApp.swift
+++ QuarterMaster/AppManagement/QuarterMasterApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct QuarterMasterApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            QuarterlyInput.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
--- QuarterMaster/Data Models/QuarterlyInput.swift
+++ QuarterMaster/Data Models/QuarterlyInput.swift
@@ -11,9 +11,25 @@ import SwiftData
 @Model
 final class QuarterlyInput {
     var timestamp: Date
-    var quarterID: String
+    var quarterID: String = ""
+    var pensionAnnuities : Double = 0.0
+    var socialSecurity : Double = 0.0
+    var interest: Double = 0.0
+    var oilRoyalties: Double = 0.0
+    var drugTrialCompensation: Double = 0.0
+    var ordinaryDividends: Double = 0.0
+    var qualifiedDividends: Double = 0.0
+    var iraDistributions: Double = 0.0
+    var shortTermCG: Double = 0.0
+    var longTermCG: Double = 0.0
+    var shortTermGain: Double = 0.0
+    var longTermGain: Double = 0.0
+    var cashDonations: Double = 0.0
+    var IRSCYWitholding: Double = 0.0
+    var IRSCYEstimates: Double = 0.0
+    var StateCYWitholding: Double = 0.0
+    var StateCYEstimates: Double = 0.0
     
-    @Relationship(deleteRule: .cascade) var fields: [InputDataFields] = []
     
     init(timestamp: Date = .now) {
         self.timestamp = timestamp
